a4d4d39d1261d83646efe94c7740c50e3e31c14d
Olson <olsonkj1@leidos.com>
2016-11-01 13:14:36 -0400

    latest

---
+++ index.php
@@ -0,0 +1,386 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Kevin Olson</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!--my stylesheet -->
+    <link rel="stylesheet" href="kevstyles.css" type="text/css">
+    <!-- JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/parallax.js"></script>
+  </head>
+  <body>
+
+
+
+
+  
+
+<!--     MY NAV BAR -->
+
+  <!-- navigation panel -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-main">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#" style="font-family: 'Trajan Pro Bold';">Kevin Olson</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="navbar-collapse-main"style=>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#music">Music</a></li>
+        <li><a href="#journey">Journey</a></li>
+        <li><a href="#videos">Videos</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+<!-- first section - Home -->
+      <div id="home" class="home">
+      <div class="text-vcenter">
+        <h1 id="mainTitle">Kevin olson musiC</h1>
+        
+        <a href="#about" class="btn btn-default btn-lg">WelcomE</a>
+
+      </div>
+    </div>
+    <!-- /first section -->
+
+    <!-- second section - Welcome -->
+    <div id="about" class="pad-section" data-speed="4" data-type="background">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <img src="images/artsy.jpg" alt="" />
+          </div>
+          <div class="col-sm-6 text-center">
+            <h2>Kevin olson musiC</h2>
+            <p class="lead">Musician and Software Developer from Vienna, Virginia.  I graduated from Virginia Tech
+            with a degree in Computer Science.  This is my official website that I created myself.</p>
+
+         <!--    <a href="http://www.facebook.com/kevinolsonmusic" target="_blank"><img src="images/facebookicon.png"></a> -->
+
+            <p class="lead"><a href="http://www.facebook.com/kevinolsonmusic" target="_blank">Facebook</a> 
+            & 
+            <a href="https://www.instagram.com/kjo133/" target="_blank">Instagram</a> </p>
+
+          </div>      
+        </div>
+
+        <br />
+        <br />
+        <h2 style="text-align: center;">Upcoming Shows</h2>
+        <a style="color:white;" href="http://jamminjava.com/event/1016783-relay-palooza-vienna/" target="_blank"><h3 style="text-align: center;">January 31 : Jammin' Java, Vienna VA</h3></a>
+        <a style="color:whitfe;" href="http://ev12.evenue.net/cgi-bin/ncommerce3/SEGetEventInfo?ticketCode=GS%3AVTECH%3AB15%3A18%3A&linkID=vtech&shopperContext=&pc=&caller=&appCode=&groupCode=BBI&cgc=" target="_blank"><h3 style="text-align: center;">March 5 : Miami vs Virginia Tech Halftime, Blacksburg VA</h3></a>
+        <a style="color:white;" href="http://www.gypsysallys.com/calendar/" target="_blank"><h3 style="text-align: center;">March 17 : Gypsy Sally's Vinyl Lounge, Washington DC</h3></a>
+
+        <br />f
+        <br />
+        <div class="flex-video widescreen"><iframe src="https://www.youtube.com/embed/ZXjWtk-RoYE?rel=0&controls=0&showinfo=0" frameborder="0" allowfullscreen></iframe></div>
+        
+      </div>
+    </div>
+<!-- /second section -->
+
+
+  <!-- third section - Music -->
+<div id="music" class="pad-section">
+    <h2 class="text-center">My Music</h2> <hr />
+
+
+  <div class="container">
+
+      <div class="col-md-6">
+        <div class="panel panel-default">
+         <div class="panel-heading">
+            <h2 class="panel-title">Blacksburg Busking</h2>
+          </div>
+          <div class="panel-body soundcloud">
+      <iframe style="border: 0; width: 100%; height: 120px;" src="http://bandcamp.com/EmbeddedPlayer/album=2410687255/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=441665611/transparent=true/" seamless><a href="http://kevinolson.bandcamp.com/album/acoustic-sounds">Acoustic Sounds by Kevin Olson</a></iframe>
+          </div>
+         </div>
+      </div>
+
+
+        <div class="col-md-6">
+        <div class="panel panel-default">
+        <div class="panel-heading">
+            <h2 class="panel-title">Meteor Shower</h2>
+          </div>
+          <div class="panel-body soundcloud">
+              <iframe style="border: 0; width: 100%; height: 120px;" src="http://bandcamp.com/EmbeddedPlayer/album=2410687255/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=1000830971/transparent=true/" seamless><a href="http://kevinolson.bandcamp.com/album/acoustic-sounds">Acoustic Sounds by Kevin Olson</a></iframe>
+          </div>
+         </div>
+      </div>
+
+      <div class="col-md-6">
+        <div class="panel panel-default">
+        <div class="panel-heading">
+            <h2 class="panel-title">Consellation</h2>
+          </div>
+          <div class="panel-body soundcloud">
+         <iframe style="border: 0; width: 100%; height: 120px;" src="http://bandcamp.com/EmbeddedPlayer/album=2410687255/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=2897942800/transparent=true/" seamless><a href="http://kevinolson.bandcamp.com/album/acoustic-sounds">Acoustic Sounds by Kevin Olson</a></iframe>
+
+          </div>
+         </div>
+      </div>
+
+      <div class="col-md-6">
+        <div class="panel panel-default">
+        <div class="panel-heading">
+            <h2 class="panel-title">66 to 81 to 460</h2>
+          </div>
+          <div class="panel-body soundcloud">
+    <iframe style="border: 0; width: 100%; height: 120px;" src="http://bandcamp.com/EmbeddedPlayer/album=2410687255/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=1860326390/transparent=true/" seamless><a href="http://kevinolson.bandcamp.com/album/acoustic-sounds">Acoustic Sounds by Kevin Olson</a></iframe>
+          </div>
+         </div>
+      </div>
+     <!--  <div class="bandcamp">
+      <iframe style="border: solid blue 2px; width: 70%; height: 120px; margin: 0 auto; background: white;" src="http://bandcamp.com/EmbeddedPlayer/album=2410687255/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=441665611/transparent=true/" seamless><a href="http://kevinolson.bandcamp.com/album/acoustic-sounds">Acoustic Sounds by Kevin Olson</a></iframe>
+
+      <iframe style="border: 0; width: 100%; height: 120px;" src="http://bandcamp.com/EmbeddedPlayer/album=2410687255/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=1000830971/transparent=true/" seamless><a href="http://kevinolson.bandcamp.com/album/acoustic-sounds">Acoustic Sounds by Kevin Olson</a></iframe>
+
+      <iframe style="border: 0; width: 100%; height: 120px;" src="http://bandcamp.com/EmbeddedPlayer/album=2410687255/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=2897942800/transparent=true/" seamless><a href="http://kevinolson.bandcamp.com/album/acoustic-sounds">Acoustic Sounds by Kevin Olson</a></iframe>
+
+      <iframe style="border: 0; width: 100%; height: 120px;" src="http://bandcamp.com/EmbeddedPlayer/album=2410687255/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=1860326390/transparent=true/" seamless><a href="http://kevinolson.bandcamp.com/album/acoustic-sounds">Acoustic Sounds by Kevin Olson</a></iframe>
+
+      </div> -->
+  </div>
+</div>
+<!-- /third section -->
+
+
+  <!-- third section - Story -->
+<div id="journey" class="pad-section">
+  <div class="container">
+    <h2 class="text-center">My Journey</h2> <hr />
+
+   
+
+<!--  <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+    <!--   <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol> -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+              <div class="description">
+                <h3>September 2014 </h3>
+                <p>My musical journey began on the streets of Blacksburg, VA at the start of my senior year at Virginia Tech.
+                I would set up with my acoustic guitar and 5W battery powered amplifer and play for an hour or so about once a month. 
+                Good size crowds would gather around and tip me as I played.  It was awesome to see that people were interested in
+                what I was creating.  My spot in front of Moes at the corner of College Avenue will always be a special place to me. </p>
+              </div>
+            </div>
+            <div class="item">
+              <div class="description">
+               <h3>November 2014</h3>
+                <p>My early street performances led to me headlining a halftime show at the Virginia Tech Men's Basketball game.  This was my 
+                first live performance on any kind of stage.  So cool to hear my guitar play over the loud PA System and capitvate a large audience.  
+                Also headlined a Virginia Tech Women's basketball game in December</p>
+              </div>
+            </div>
+            <div class="item">
+              <div class="description">
+               <h3>December 2014</h3>
+                <p>First shot at a music venue came at Jammin' Java in my hometown Vienna, VA.  I was given a 20 minute set to open up for Deanna Bogart and her band.
+                This was my first time playing a long set list and at an event solely for music.  Great time making my Vienna debut.</p>
+              </div>
+            </div>
+            <div class="item">
+              <div class="description">
+                <h3>April 2015</h3>
+                <p>I was given a slot to play at Virginia Tech's Relay for Life event.  It was so amazing to be a part of a great cause.  Very surreal experience being 
+                able to play my guitar on the famous Drillfield in front of the iconic Burruss Hall.  This was the coolest stage I have played on so far. </p>
+              </div>
+            </div>
+            <div class="item">
+              <div class="description">
+                <h3>May 2015</h3>
+                <p>Asked to perform at Blacksburg's Fork and Cork Festival.  Great to be a part of the entertainment and sample wine with friends.  </p>
+              </div>
+            </div>
+            <div class="item">
+              <div class="description">
+                <h3>June 2015</h3>
+                <p>I got to share my guitar routine with people at the Celebrate Fairfax Fair.  This was my first big event with big name bands such as Kongos and 3 Doors Down.</p>
+              </div>
+            </div>
+            <div class="item">
+              <div class="description">
+                <h3>July 2015</h3>
+                <p>Opened up for Deanna Bogart for the second time in a year a Jammin' Java.  Always fun to play infront of hometown family and friends.  </p>
+              </div>
+            </div>
+             <div class="item">
+              <div class="description">
+                <h3>November 2015</h3>
+                <p>Headlined the halftime show of a Virginia Tech Women's Basketball game.  I combined the songs I play during my longer set into one 6 minute medley</p>
+              </div>
+            </div>
+
+
+        </div>
+
+
+
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel --> 
+
+
+
+
+
+
+<!--     
+   
+  
+  
+ 
+   
+  
+   
+
+ -->
+  </div>
+</div>
+<!-- /third section -->
+
+
+
+
+<!-- fourth section - Information -->
+<div id="videos" class="pad-section">
+  <div class="container">
+   <h2 class="text-center" id="videoTitle">Live Performances</h2> <hr />
+    <div class="row">
+
+      <div class="col-sm-6">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h2 class="panel-title">Virginia Tech Halftime 2014</h2>
+          </div>
+          <div class="panel-body lead">
+            <div class="videos flex-video widescreen">
+              <iframe src="https://www.youtube.com/embed/qAqlLcVBloo?rel=0&controls=1&showinfo=0" frameborder="0" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h2 class="panel-title">VT Relay for Life 2015</h2>
+          </div>
+          <div class="panel-body lead">
+            <div class="videos flex-video widescreen">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLJ-J2MKdCNr_xTkqqHMzeFia5VF77-FBm" frameborder="0" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h2 class="panel-title">Jammin' Java 2015</h2>
+          </div>
+          <div class="panel-body lead">
+            <div class="videos flex-video widescreen">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/PcNm-AXw6Ac" frameborder="0" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+       <div class="col-sm-6">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h2 class="panel-title">Jammin' Java 2014</h2>
+          </div>
+          <div class="panel-body lead">
+            <div class="videos flex-video widescreen">
+             <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLJ-J2MKdCNr8NseyxIBvbubrYsu1YZuEB" frameborder="0" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+  </div>
+
+</div>
+
+
+
+
+
+<!-- /fourth section -->
+
+<!-- fifth section -->
+<!-- <div id="services" class="pad-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <h3>Parallax scrolling effect is in action</h3>
+        <h4>The next is the address on Google maps</h4>
+      </div>
+    </div>
+  </div>
+</div> -->
+<!-- /fifth section -->
+
+
+
+
+
+
+<!-- footer -->
+<footer>
+
+  <div class="container">
+    <p class="text-right">kevinolsonmusic@gmail.com</p>
+
+
+  </div>
+</footer>
+
+
+<!-- /footer -->
+
+
+
+  </body>
+</html>
